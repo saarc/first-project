6f1aeea290ebe5662aa67f4f202a8335ca9ef348
saarc <saarc99@naver.com>
2022-04-21 11:17:02 +0900

    Update install.sh

--- contract/simpleasset/1.0/install.sh
+++ contract/simpleasset/1.0/install.sh
@@ -3,9 +3,11 @@ set -x
 
 # 1. 설치
 docker exec cli peer chaincode install -n simpleasset -v 1.0 -p github.com/simpleasset/1.0
+docker exec cli peer chaincode list --installed
 # 2. 업그래이드
 docker exec cli peer chaincode instantiate -n simpleasset -v 1.0 -c '{"Args":["a","100"]}' -C mychannel -P 'AND ("Org1MSP.member")'
 sleep 3
+docker exec cli peer chaincode list --instantiated -C mychannel
 # 3. 인보크 
 docker exec cli peer chaincode invoke -n simpleasset -C mychannel -c '{"Args":["set","b","200"]}'
 sleep 3
